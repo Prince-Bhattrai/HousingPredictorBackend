202958bf5f2da82e33684b12b0856a97e82f13d1
Prince Bhattrai <bhattraiprince39@gmail.com>
2025-08-16 19:57:56 +0545

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://housing-predictor-ebon.vercel.app/",
+    "https://housing-predictor-ebon.vercel.app",
 ]
 
 app.add_middleware(
